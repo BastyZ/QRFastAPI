1d2c18b954141e42559b831e2838c74db4be10d4
BastyZ <bastian.inostroza@gmail.com>
2020-12-22 17:52:34 -0300

    Added QR code creation capability using Pedro Script

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import io
 from typing import Optional
 
-from fastapi import FastAPI
-from fastapi.responses import FileResponse
+import qrcode
+from fastapi import FastAPI, Response
 
 app = FastAPI()
 
@@ -13,4 +14,19 @@ def read_root():
 
 @app.get("/QR")
 def generate_qr(form_id: int, q: Optional[str] = None):
-    return FileResponse("example.jpg")
+    # Create instance of qrcode
+    qr = qrcode.QRCode(
+        version=1,
+        box_size=10,
+        border=5
+    )
+    # Give parameter from GET
+    qr.add_data(form_id)
+    qr.make(fit=True)
+    img = qr.make_image(fill='black', back_color='white')
+    # Buffer to save the image on RAM
+    buffer = io.BytesIO()
+    img.save(buffer, "PNG")
+    response_img = buffer.getvalue()
+    # Return QR code as image
+    return Response(content=response_img, media_type="image/png")
